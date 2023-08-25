d779923e72bad0efae87a74fa203e3e354b3a837
AMIN <aminmedfai30@gmail.com>
2023-08-25 22:46:56 +0100

    Create coll.c

---
+++ coll.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int collatz(int x) {
+    if (x % 2 == 0) {
+        return x / 2;
+    } else {
+        return (x * 3) + 1;
+    }
+}
+
+int main(void) {
+    int x = 863323;
+
+    if (x <= 0) {
+        printf("Input must be a positive integer.\n");
+        return 1;
+    }
+
+    do {
+        printf("%d\n", x);
+        x = collatz(x);
+    } while (x != 1);
+
+    printf("%d\n", x); // Print the final 1
+
+    return 0;
+}
+
